787ace843657fccfe03921f1917ed1b8937b783f
Joe Czubiak <jcz530@uw.edu>
2013-09-03 16:08:37 -0700

    added school links activity

--- src/co/localism/losal/activities/SchoolLinksActivity.java
+++ src/co/localism/losal/activities/SchoolLinksActivity.java
@@ -3,21 +3,34 @@ package co.localism.losal.activities;
 import com.jeremyfeinstein.slidingmenu.lib.SlidingMenu;
 
 import co.localism.losal.R;
+import co.localism.losal.SVGHandler;
 import co.localism.losal.SetUpSlidingMenu;
 import co.localism.losal.listens.PersonalOptionsOnClickListeners;
 import android.app.ActionBar;
 import android.app.Activity;
+import android.content.Intent;
 import android.graphics.drawable.ColorDrawable;
 import android.graphics.drawable.Drawable;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
-public class SchoolLinksActivity extends Activity{
+public class SchoolLinksActivity extends Activity implements OnClickListener{
 	private SlidingMenu sm;
+	private String FLICKR = "http://www.flickr.com/photos/97669165@N06/";
+	private String INSTA = "http://instagram.com/LosAlamitosHigh";
+	private String FB = "http://www.facebook.com/losalamitoshighschool";
+	private String TW = "https://twitter.com/LosAlamitosHigh";
+	private String GRIFFIN_NEWS = "http://www.youtube.com/user/GriffinNews2013";
+	private String ARNOLD_YOUTUBE = "http://www.youtube.com/user/josharnold67"; 
+	
     @Override
 	public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -38,10 +51,37 @@ public class SchoolLinksActivity extends Activity{
 		new PersonalOptionsOnClickListeners(
 				(LinearLayout) findViewById(R.id.po), this);
 
-
-        
-        
-    }
+		ImageView iv = new SVGHandler().svg_to_imageview(this, R.raw.social_links_instagram);
+		LinearLayout ll = (LinearLayout) findViewById(R.id.sl_insta);
+		ll.addView(iv, 0);
+		ll.setOnClickListener(this);
+		
+		ImageView iv2 = new SVGHandler().svg_to_imageview(this, R.raw.social_links_facebook);
+		LinearLayout ll2 = (LinearLayout) findViewById(R.id.sl_fb);
+		ll2.addView(iv2, 0);
+		ll2.setOnClickListener(this);
+		
+		ImageView iv3 = new SVGHandler().svg_to_imageview(this, R.raw.social_links_twitter);
+		LinearLayout ll3 = (LinearLayout) findViewById(R.id.sl_tw);
+		ll3.addView(iv3, 0);
+		ll3.setOnClickListener(this);
+		
+		ImageView iv4 = new SVGHandler().svg_to_imageview(this, R.raw.social_links_flicker);
+		LinearLayout ll4 = (LinearLayout) findViewById(R.id.sl_flickr);
+		ll4.addView(iv4, 0);
+		ll4.setOnClickListener(this);
+		
+		ImageView iv5 = new SVGHandler().svg_to_imageview(this, R.raw.social_links_youtube);
+		LinearLayout ll5 = (LinearLayout) findViewById(R.id.sl_yt);
+		ll5.addView(iv5, 0);
+		ll5.setOnClickListener(this);
+		
+		ImageView iv6 = new SVGHandler().svg_to_imageview(this, R.raw.social_links_youtube);
+		LinearLayout ll6 = (LinearLayout) findViewById(R.id.sl_yt_2);
+		ll6.addView(iv6, 0);
+		ll6.setOnClickListener(this);
+		
+  }
 
     
     
@@ -61,6 +101,50 @@ public class SchoolLinksActivity extends Activity{
 			break;
 		}
 		return super.onOptionsItemSelected(item);
+	}
+
+
+
+
+	@Override
+	public void onClick(View v) {
+		switch(v.getId()){
+		case R.id.sl_fb:
+			openURL(FB);			
+			break;
+		case R.id.sl_tw:
+			openURL(TW);			
+			break;
+			
+		case R.id.sl_insta:
+			openURL(INSTA);			
+			break;
+			
+		case R.id.sl_flickr:
+			openURL(FLICKR);			
+			break;
+		case R.id.sl_yt:
+			openURL(GRIFFIN_NEWS);			
+			break;
+		case R.id.sl_yt_2:
+			openURL(ARNOLD_YOUTUBE);			
+			break;
+			
+		}
+		
+		
+		
+		
+	}
+	public void openURL(String URL) {
+		if (URL.length() > 0) {
+			try {
+				Intent openURL = new Intent(Intent.ACTION_VIEW, Uri.parse(URL));
+				this.startActivity(openURL);
+			} catch (Exception e) {
+
+			}
+		}
 	}
 
     

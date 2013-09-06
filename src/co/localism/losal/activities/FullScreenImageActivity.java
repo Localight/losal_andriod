8feb1f06af73a382b15c2111047b96e11d9adb66
Joe Czubiak <jcz530@uw.edu>
2013-09-06 13:34:33 -0700

    added onclick to fullscreen to exit

--- src/co/localism/losal/activities/FullScreenImageActivity.java
+++ src/co/localism/losal/activities/FullScreenImageActivity.java
@@ -4,6 +4,8 @@ import co.localism.losal.R;
 import co.localism.losal.adapters.PostAdapter;
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
 import android.widget.ImageView;
 /**
  * This activity displays an image at full screen when it is called from PostAdapter.class
@@ -23,8 +25,16 @@ public class FullScreenImageActivity extends Activity {
 	        
 	        ImageView iv = (ImageView) findViewById(R.id.iv_fullscreen);
 //	        iv.setImageResource(extras.getInt("imageID"));
-			
 	        PostAdapter.mImageLoader.displayImage(extras.getString("imageURL"), iv);
+	        iv.setOnClickListener(new OnClickListener(){
+
+				@Override
+				public void onClick(View v) {
+					finish();
+					
+				}
+	        	
+	        });
 //	        TODO: make sure that it scales the image correctly and leaves black space if needed. 
 //	        For Example: A square image should scale so that the image fits to the edge horizontally
 //	        but has black space on the top and bottom. Assuming the phone is being held in portrait mode.
